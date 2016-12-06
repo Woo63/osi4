1a0bbe5b06b0a4f3d3029660dadcc71f4c45024a
Woo63 <55555r@list.ru>
2016-12-06 23:03:29 +0400

    Update osi.cpp

--- osi.cpp
+++ osi.cpp
@@ -10,7 +10,7 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <fcntl.h>
-#define PROIZSEM "/proizsem" //Названия для семафоров епт которые нужны для синхронизации
+#define PROIZSEM "/proizsem" 
 #define RAZSEM "/razsem"
 #define SQRTSEM "/sqrtsem"
 #define DELSEM "/delsem"
@@ -25,14 +25,14 @@ int proizsock, razsock, sqrtsock, delsock, mysock; //сокеты
 char buf[BUFSIZE]; //буффер
 struct sockaddr_in addr1,addr2,addr3,addr4; //структуры адресов сокетов
 int bytes_read, total = 0; //количество прочитаных из сокета байтов и общее число прочитаных байтов
-pid_t p1, p2, p3, p4; //процессов адреса
-using namespace std; //нухз
+pid_t p1, p2, p3, p4; 
+using namespace std; 
 int main() {
-  sem_t *sqrts = sem_open(SQRTSEM, O_CREAT, 0644, 0); //создаём семафоры
+  sem_t *sqrts = sem_open(SQRTSEM, O_CREAT, 0644, 0); 
   sem_t *raz = sem_open(RAZSEM, O_CREAT, 0644, 0);
   sem_t *proiz = sem_open(PROIZSEM, O_CREAT, 0644, 0);
   sem_t *del = sem_open(DELSEM, O_CREAT, 0644, 0);
-  p1=fork(); //форкаем процессы, крч. Чтобы не открывать их руками
+  p1=fork(); 
   p2=fork();
   p3=fork();
   p4=fork();
@@ -58,30 +58,30 @@ int main() {
   else //иначе основной процесс
   {
   mysock = socket(AF_INET, SOCK_DGRAM, 0); //создаём сокет
-  addr5.sin_family = AF_INET; //хуярим адреса. я не знаю как это работает, даже не спрашивай
-  addr5.sin_port = htons(PORT); //тут крч порт сокета пишем в структуру адреса
-  addr5.sin_addr.s_addr = inet_addr(MYSOCK); // тут крч получаем адрес из строки
+  addr5.sin_family = AF_INET; 
+  addr5.sin_port = htons(PORT); 
+  addr5.sin_addr.s_addr = inet_addr(MYSOCK); 
   bind(mysock, (struct sockaddr *)&addr5, sizeof(addr5)); //забиваем сокет для того, чтобы его слушать.
-  //init sock for sqr
-  proizsock = socket(AF_INET, SOCK_DGRAM, 0); //тут хуярим сокет для процесса, чтоб в него писать.
-  addr1.sin_family = AF_INET;//всё то же самое
+ 
+  proizsock = socket(AF_INET, SOCK_DGRAM, 0); 
+  addr1.sin_family = AF_INET;
   addr1.sin_port = htons(PORT);
   addr1.sin_addr.s_addr = inet_addr(SQIP);
-  connect(proizsock, (struct sockaddr *)&addr1, sizeof(addr1));//только тут коннектимся к нему, а не создаём.
-  //init sock for sum
-  razsock = socket(AF_INET, SOCK_DGRAM, 0); // и так ещё пару раз хуярим
+  connect(proizsock, (struct sockaddr *)&addr1, sizeof(addr1));
+  
+  razsock = socket(AF_INET, SOCK_DGRAM, 0); 
   addr2.sin_family = AF_INET;
   addr2.sin_port = htons(PORT);
   addr2.sin_addr.s_addr = inet_addr(SUMIP);
   connect(razsock, (struct sockaddr *)&addr2, sizeof(addr2));
-  //init sock for sqrt
+  
   sqrtsock = socket(AF_INET, SOCK_DGRAM, 0);
   addr3.sin_family = AF_INET;
   addr3.sin_port = htons(PORT);
   addr3.sin_addr.s_addr = inet_addr(SQRTIP);
   connect(sqrtsock, (struct sockaddr *)&addr3, sizeof(addr3));
 
-  delsock = socket(AF_INET, SOCK_DGRAM, 0); // и так ещё пару раз хуярим
+  delsock = socket(AF_INET, SOCK_DGRAM, 0);
   addr4.sin_family = AF_INET;
   addr4.sin_port = htons(PORT);
   addr4.sin_addr.s_addr = inet_addr(SUMIP);
@@ -89,32 +89,32 @@ int main() {
 
   float a,b,c, b2, 4a, 4ac, d;
   cout << "Input 3 numbers greater then zero.\n";
-  cin >>a >> b>> c; // читаем ебучие числа
+  cin >>a >> b>> c; 
   if (a<=0 || b<=0)
 
   cout << "A: " << a << " B: " << b << "C:" << c << endl;
   c=4*c;
   cout << "Sending a" << endl;
-  sem_post(proiz); //открываем семафор чтоб нахуй сокет начал слушать
-  send(proizsock, &b, sizeof(b), 0); //отправляем блядские числа
-  send(proizsock, &b, sizeof(b), 0); //отправляем блядские числа
+  sem_post(proiz); //открываем семафор 
+  send(proizsock, &b, sizeof(b), 0);
+  send(proizsock, &b, sizeof(b), 0);
   bytes_read=recvfrom(mysock, &b2, sizeof(float), 0, NULL, NULL); //читаем результат
   total+=bytes_read;
   cout << "Reciving b2 " << b2 << endl;
   cout << "Sending b2" << endl;
 
-  sem_post(proiz); //открываем семафор чтоб нахуй сокет начал слушать
-  send(proizsock, &a, sizeof(a), 0); //отправляем блядские числа
-  send(proizsock, &c, sizeof(c), 0); //отправляем блядские числа
-  bytes_read=recvfrom(mysock, &c, sizeof(float), 0, NULL, NULL); //читаем результат
+  sem_post(proiz); 
+  send(proizsock, &a, sizeof(a), 0);
+  send(proizsock, &c, sizeof(c), 0); 
+  bytes_read=recvfrom(mysock, &c, sizeof(float), 0, NULL, NULL); 
   total+=bytes_read;
   cout << "Reciving 4ac " << c << endl;
   cout << "Sending 4ac" << endl;
 
-  sem_post(raz); //тут нахуй сумму блять будем считать
-  send(razsock, &b2, sizeof(b2), 0); //отправляем первое число
-  send(razsock, &c, sizeof(c), 0); //и второе
-  bytes_read=recvfrom(mysock, &d, sizeof(float), 0, NULL, NULL); //читаем ебучий результат
+  sem_post(raz);
+  send(razsock, &b2, sizeof(b2), 0); 
+  send(razsock, &c, sizeof(c), 0); 
+  bytes_read=recvfrom(mysock, &d, sizeof(float), 0, NULL, NULL); 
   cout << "Reciving disk " << d << endl;
   total+=bytes_read;
   cout << "Sending raz" << endl;
@@ -125,42 +125,40 @@ int main() {
   cout << "Reciving sqrt " << d << endl;
   total+=bytes_read;
 
-  sem_post(raz); //тут нахуй сумму блять будем считать
-  send(razsock, &d, sizeof(d), 0); //отправляем первое число
-  send(razsock, &b, sizeof(b), 0); //и второе
-  bytes_read=recvfrom(mysock, &x1, sizeof(float), 0, NULL, NULL); //читаем ебучий результат
-  //cout << "Reciving disk " << d << endl;
+  sem_post(raz); 
+  send(razsock, &d, sizeof(d), 0);
+  send(razsock, &b, sizeof(b), 0); 
+  bytes_read=recvfrom(mysock, &x1, sizeof(float), 0, NULL, NULL);  
   total+=bytes_read;
-  //cout << "Sending raz" << endl;
+  
   a=2*a;
   d=-d;
 
-  sem_post(raz); //тут нахуй сумму блять будем считать
-  send(razsock, &d, sizeof(d), 0); //отправляем первое число
-  send(razsock, &b, sizeof(b), 0); //и второе
-  bytes_read=recvfrom(mysock, &x2, sizeof(float), 0, NULL, NULL); //читаем ебучий результат
-  //cout << "Reciving disk " << d << endl;
+  sem_post(raz); 
+  send(razsock, &d, sizeof(d), 0); 
+  send(razsock, &b, sizeof(b), 0); 
+  bytes_read=recvfrom(mysock, &x2, sizeof(float), 0, NULL, NULL); 
+ 
   total+=bytes_read;
-  //cout << "Sending raz" << endl;
-
-  sem_post(del); //открываем семафор чтоб нахуй сокет начал слушать
-  send(proizsock, &x1, sizeof(x1), 0); //отправляем блядские числа
-  send(proizsock, &a, sizeof(a), 0); //отправляем блядские числа
-  bytes_read=recvfrom(mysock, &x1, sizeof(float), 0, NULL, NULL); //читаем результат
+ 
+  sem_post(del); 
+  send(proizsock, &x1, sizeof(x1), 0);
+  send(proizsock, &a, sizeof(a), 0);
+  bytes_read=recvfrom(mysock, &x1, sizeof(float), 0, NULL, NULL); 
   total+=bytes_read;
   cout << "Reciving x1 " << x1 << endl;
   cout << "Sending x1" << endl;
 
-  sem_post(del); //открываем семафор чтоб нахуй сокет начал слушать
-  send(proizsock, &x2, sizeof(x2), 0); //отправляем блядские числа
-  send(proizsock, &a, sizeof(a), 0); //отправляем блядские числа
-  bytes_read=recvfrom(mysock, &x2, sizeof(float), 0, NULL, NULL); //читаем результат
+  sem_post(del); 
+  send(proizsock, &x2, sizeof(x2), 0); 
+  send(proizsock, &a, sizeof(a), 0); 
+  bytes_read=recvfrom(mysock, &x2, sizeof(float), 0, NULL, NULL); 
   total+=bytes_read;
   cout << "Reciving x2 " << x2 << endl;
   cout << "Sending x2" << endl;
 
 
-  cout << "RESULT: " << x1 << x2 << endl; //выводим его, нахуй. мы охуенны. давайте выпьем сока.
+  cout << "RESULT: " << x1 << x2 << endl; 
   cout << "TOTAL BYTES READ: " << total << endl;
   return 0;
 }
