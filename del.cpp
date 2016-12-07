af202eb4f3d6f823c8ae6975100b0971f197ce83
Woo63 <55555r@list.ru>
2016-12-07 20:36:23 +0400

    Update del.cpp

--- del.cpp
+++ del.cpp
@@ -18,7 +18,7 @@ volatile int flag = 0;
 struct sockaddr_in addr, addr1;
 
 int main() {
-  sem_t *sum = sem_open(DELSEM, 0);
+  sem_t *del = sem_open(DELSEM, 0);
  sock = socket(AF_INET, SOCK_DGRAM, 0);
  sock1 = socket(AF_INET, SOCK_DGRAM, 0);
  addr.sin_family = AF_INET;
@@ -30,7 +30,7 @@ int main() {
  addr1.sin_addr.s_addr = inet_addr("127.0.0.5");
  connect(sock1, (struct sockaddr *)&addr1, sizeof(addr1));
  while(1) {
-   sem_wait(sum);
+   sem_wait(del);
    std::cout << "WAITING FOR SIGNAL in del thread" << std::endl;
    std::cout << "WAITING FOR FIRST NUM...\n";
    bytes_read = recvfrom(sock, &num1, sizeof(float), 0, NULL, NULL);
