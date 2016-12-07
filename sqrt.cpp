720a84f873dfb0b108a6db9216b4e3e2f9eb1069
Woo63 <55555r@list.ru>
2016-12-07 22:23:08 +0400

    Update sqrt.cpp

--- sqrt.cpp
+++ sqrt.cpp
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#define SQRTSEM "/sqrtsem" 
+#define SQRTSEM "/sqrtsem"
 #define PORT 6969
 #define BUFSIZE 32
 float num;
@@ -20,10 +20,10 @@ int sock, sock1;
 struct sockaddr_in addr, addr1;
 
 int main() {
- sem_t *sqrts = sem_open(SQRTSEM, 0); 
+ sem_t *sqrts = sem_open(SQRTSEM, 0);
  sock = socket(AF_INET, SOCK_DGRAM, 0); //создаём сокеты
  sock1 = socket(AF_INET, SOCK_DGRAM, 0);
- addr.sin_family = AF_INET; 
+ addr.sin_family = AF_INET;
  addr.sin_port = htons(PORT);
  addr.sin_addr.s_addr = inet_addr("127.0.0.3");
  bind(sock, (struct sockaddr *)&addr, sizeof(addr));
@@ -32,15 +32,13 @@ int main() {
  addr1.sin_addr.s_addr = inet_addr("127.0.0.5");
  connect(sock1, (struct sockaddr *)&addr1, sizeof(addr1));
  while(1) {
-   sem_wait(sqrts); 
+   sem_wait(sqrts);
    std::cout << "WAITING FOR DATA...\n";
-   bytes_read = recvfrom(sock, &num, sizeof(float), 0, NULL, NULL); 
-   total = total + bytes_read;
+   bytes_read = recvfrom(sock, &num, sizeof(float), 0, NULL, NULL);
    std::cout << "Received " << num << std::endl;
-   float res = sqrt(num); 
+   float res = sqrt(num);
    fflush(stdout);
-   std::cout << "Sending data back...\n";
-   send(sock1, &res, sizeof(res), 0); 
+   send(sock1, &res, sizeof(res), 0);
  }
  return 0;
 }
